4e294b0b2a31b6b9b2c9b5308b81bd3e724909b7
MLK276 <21904808@etu.unicaen.fr>
2020-06-10 23:35:07 +0200

    tp4

---
+++ app/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title><TP4></title>
+	<meta charset="utf-8">
+	<meta name="description" content="TP4">
+	<meta name="author" content="Malek Mammeri">
+	
+<style>
+.bouton1{
+background-color: #21ba45;
+color: white;
+padding: 10px;
+border: none;
+}
+.bouton2{
+background-color: #00b5ad;
+color: white;
+padding: 10px;
+border: none;
+}
+</style> 
+
+</head>
+<body>
+
+<?php
+echo '<img src="images/logo.png" border="0" /></div> ';
+?>
---
+++ app/index.php
@@ -0,0 +1,28 @@
+<?php
+include "header.php";
+?>
+
+<table border=0 cellspacing=0 cellpadding=0>
+ <tr>
+  <td width=75>
+  <p><img width=75 height=75
+  src="images/mob.png"></p>
+  </td>
+  <td width=400>
+
+<h2>Bienvenue&nbsp;!</h2>
+
+  <p>Cette application vous permet de gérer vos flashmobs.</p>
+
+
+<a href="./flashmobs"><input type="button" value="Liste des flash-mobs" class="bouton1"></a>
+<a href="./flashmobs/create"><input type="button" value="+ Créer une flash-mob" class="bouton2"></a>
+
+  </td>
+ </tr>
+</table>
+
+
+<?php
+include "footer.php";
+?>

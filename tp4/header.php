70cc44827fc1c9dfd47fb0c124459f03eb17b2f4
MLK276 <21904808@etu.unicaen.fr>
2020-06-10 16:28:55 +0200

    tp4

---
+++ tp4/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title><TP4></title>
+	<meta charset="utf-8">
+	<meta name="description" content="TP4">
+	<meta name="author" content="Malek Mammeri">
+	
+
+</head>
+<body>
+
+<?php
+echo '<img src="images/logo.png" border="0" /></div> ';
+?>
---
+++ tp4/index.php
@@ -0,0 +1,10 @@
+<?php
+include "header.php";
+?>
+
+<h2>Bienvenue</h2>
+
+
+<?php
+include "footer.php";
+?>

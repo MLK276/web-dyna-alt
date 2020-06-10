76a3c6b133012ccde159b6dd7f6cf50e3db5818a
MLK276 <21904808@etu.unicaen.fr>
2020-06-10 17:09:34 +0200

    tp4

--- tp4/index.php
+++ tp4/index.php
@@ -2,9 +2,20 @@
 include "header.php";
 ?>
 
-<h2>Bienvenue</h2>
+<table border=0 cellspacing=0 cellpadding=0>
+ <tr>
+  <td width=75>
+  <p><img width=53 height=55
+  src="images/mob.png"></p>
+  </td>
+  <td width=302 style='width:8.0cm;padding:0cm 5.4pt 0cm 5.4pt'>
+<h2>Bienvenue&nbsp;!</h2>
+  <p>Cette application vous permet de gérer vos Flashmobs.</p>
+  </td>
+ </tr>
+</table>
 
-
+<input type="button" value="Liste des flash-mobs">
 <?php
 include "footer.php";
 ?>
